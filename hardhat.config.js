1e538313f3a60fba95e14dd483b3658f0f4c136c
sharath V <sharathkrml@gmail.com>
2022-03-11 11:49:47 +0530

    added hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config({ path: ".env" });
+
+const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
+
+const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
+
+module.exports = {
+  networks:{
+    hardhat:{
+      chainId:1337
+    },mumbai:{
+      url: ALCHEMY_API_KEY_URL,
+      accounts: [MUMBAI_PRIVATE_KEY]
+    }
+  },
+  solidity: "0.8.4",
+};
